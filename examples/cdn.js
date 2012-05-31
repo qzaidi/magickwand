ccec8222ec0bd96b3b9dc817bba58001ab3bfa71
Qasim Zaidi <qasim@zaidi.me>
2012-05-31 12:12:18 +0530

    a working cdn example, finally

--- examples/cdn.js
+++ examples/cdn.js
@@ -3,52 +3,64 @@
 var parse = require('url').parse;
 var magickwand = require('magickwand');
 var mime = require('mime');
+var util = require('util');
+
+function errorResponse(res) {
+  res.statusCode = 404;
+  res.setHeader('Content-Length',0);
+  res.end('<h1>404 Not Found</h1>');
+}
 
 function cdnCache(options) {
   var config = {};
 
-  config.path = options.path || '^/cache';
+  config.path = '^/' + (options.path || 'cache');
   config.mime = options.mime || '^image/';
+  config.srcPath = options.srcPath ||  "public/images/";
   config.fileTypes = options.fileTypes || [ '.jpg', '.gif', '.png' ];
   config.getParams = options.getParams || function(path) {
-    var params = path.split('/',3);
+    var params = path.match('/cache/([0-9]*)x([0-9]*)/(.*)');
     var result;
     var dims;
-    if (    (params.length == 3)
-         && (params[1].indexOf('x'))
-         && (config.fileTypes.some(function(elem) { return params[2].match(elem); })) 
+    if (    (params && params.length == 4)
+         && (config.fileTypes.some(function(elem) { return params[3].match(elem); })) 
        ) {
-      dims = params[1].split('x');    
       result = {
-        width: dims[0],
-        height: dims[1],
-        path: 'public/' + params[2]
+        width: params[1],
+        height: params[2],
+        path: config.srcPath + params[3]
       };
     }
 
     return result;
   };
+
   
   return function(req,res,next) {
     var pathname = parse(req.url).pathname;
     var resizeOpts;
 
-    if (pathname.match(config.path) == false) {
+    if (pathname.match(config.path) == null) {
       return next();
     }
 
     resizeOpts = config.getParams(req.url);
-    magickwand.resize(resizeOpts.path, resizeOpts.width, resizeOpts.height, function(err,data) {
-      if (!err && data) {
-        res.setHeader('Content-Type',mime.lookup(resizeOpts.path));
-        res.setHeader('Content-Length',data.length);
-        res.end(data,"binary");
-      } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Length',0);
-        res.end('<h1>404 Not Found</h1>');
-      }
-    });
+    if (resizeOpts) {
+      util.log('resizing image ' + req.url);
+      magickwand.resize(resizeOpts.path, resizeOpts.width, resizeOpts.height, function(err,data) {
+        if (!err && data) {
+          res.setHeader('Content-Type',mime.lookup(resizeOpts.path));
+          res.setHeader('Content-Length',data.length);
+          res.end(data,"binary");
+        } else {
+          errorResponse(res);
+        }
+      });
+    } else {
+      util.log('failed to resize image ' + req.url);
+      errorResponse(res);
+    }
   };
 }
-module.export = cdnCache;
+
+module.exports = cdnCache;
